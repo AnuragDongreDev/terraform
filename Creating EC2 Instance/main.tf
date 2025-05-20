3147addc5c05ed03de8e7b7e5faa7bb60c55f922
Anurag <anurag.dongre.jobs@gmail.com>
2025-05-20 15:47:18 +0530

    introducing the main.tf and var.tf code

--- Creating EC2 Instance/main.tf
+++ Creating EC2 Instance/main.tf
@@ -1,1 +1,12 @@
-provider 
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "example" {
+  ami           = var.ami  # Amazon Linux 2 AMI (update as per region)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "EC2_created_via_Terraform"
+  }
+}
---
+++ Creating EC2 Instance/vars.tf
@@ -0,0 +1,7 @@
+variable "region"{
+  default = "us-east-1"
+}
+
+variable "ami"{
+  default="ami-0953476d60561c955"
+}
